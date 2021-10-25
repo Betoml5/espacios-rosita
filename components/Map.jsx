5509abd41a6182f2fde7a054305b6d363c9c23aa
Beto Martinez <betoml5@hotmail.com>
2021-10-25 12:37:13 -0500

    Add simbologia and buble to toggle it

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,14 +1,12 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Image from "next/image";
 import Link from "next/link";
-import { icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import {
-  redCircleIcon,
-  orangeCircleIcon,
-  yellowCircleIcon,
-  greenCircleIcon,
-} from "../icons";
+import { redCircleIcon, orangeCircleIcon, yellowCircleIcon } from "../icons";
+import { useState } from "react";
 const Map = ({ reports }) => {
+  const [bubble, setBubble] = useState(true);
+
   const tokenMapbox = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
   const urlMap = `https://api.mapbox.com/styles/v1/betoml5/ckudbz0wj1f3s17qtv0w0cqe8/tiles/256/{z}/{x}/{y}@2x?access_token=${tokenMapbox}`;
   return (
@@ -18,6 +16,7 @@ const Map = ({ reports }) => {
         zoom={13}
         scrollWheelZoom={false}
         style={{
+          position: "relative",
           height: "80vh",
           zIndex: "0",
           borderRadius: "8px",
@@ -56,6 +55,34 @@ const Map = ({ reports }) => {
           );
         })}
       </MapContainer>
+      <div className="text-center bg-white z-50 absolute w-3/4 mx-auto left-0 right-0 self-center p-4 bottom-10 rounded-md md:w-1/4">
+        <div className={`relative ${!bubble && "hidden"}`}>
+          <div className="absolute right-0" onClick={() => setBubble(false)}>
+            X
+          </div>
+          <p className="mb-2">Simbología</p>
+          <div>
+            <div className="flex items-center">
+              <Image src="/red-circle.png" width={30} height={30} />
+              <p className="ml-2">3 Tipos de acoso</p>
+            </div>
+            <div className="flex items-center my-2">
+              <Image src="/orange-circle.png" width={30} height={30} />
+              <p className="ml-2">2 Tipos de acoso</p>
+            </div>
+            <div className="flex items-center">
+              <Image src="/yellow-circle.png" width={30} height={30} />
+              <p className="ml-2">1 Tipo de acoso</p>
+            </div>
+          </div>
+        </div>
+        <button
+          className={` ${bubble && "hidden"}`}
+          onClick={() => setBubble(true)}
+        >
+          Abrir simbología
+        </button>
+      </div>
     </div>
   );
 };
